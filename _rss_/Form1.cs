c85ec38cb86c0b139819acd722eb8724e4defa6b
Emir <emirkan.ulker9@gmail.com>
2023-12-24 15:00:08 +0300

    v

---
+++ _rss_/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace _rss_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            XmlTextReader xmloku = new XmlTextReader("http://www.haberturk.com/rss");
+            while (xmloku.Read())
+            {
+                if(xmloku.Name == "title")
+                {
+                    listBox1.Items.Add(xmloku.ReadString());
+                }
+            }
+        }
+    }
+}
